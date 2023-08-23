c0b9d385a28451d58ae54ac219e18abd0354e7c4
Janusz Tylek <j@epe.si>
2023-08-23 15:43:14 +0200

    Added URL to Epesi in body

--- HelloWorld_0.php
+++ HelloWorld_0.php
@@ -7,7 +7,7 @@ class Academy_HelloWorld extends Module { // - notice how the class name represe
   public function body() { // - modules main code
 
     print('<H1>Hello Janusz!</H1><H2>Welcome to Epesi Academy.</H2>');
-    print('<p><strong>### Lesson 1 ###</strong><br><br>Hello World in Epesi to demonstrate low-code and no-code PHP development.</p>');
+    print('<p><strong>### Lesson 1 ###</strong><br><br>Hello World in <a href="https://epe.si">Epesi</a> to demonstrate low-code and no-code PHP development.</p>');
 
     print('This is a test');
     }
