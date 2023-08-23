2796a6a8ec46ecb1d18ccab80d3fd8c1efdff625
Janusz Tylek <j@epe.si>
2023-08-23 14:02:13 +0200

    This is a test added

--- HelloWorld_0.php
+++ HelloWorld_0.php
@@ -9,6 +9,7 @@ class Academy_HelloWorld extends Module { // - notice how the class name represe
     print('<H1>Hello Janusz!</H1><H2>Welcome to Epesi Academy.</H2>');
     print('<p><strong>### Lesson 1 ###</strong><br><br>Hello World in Epesi to demonstrate low-code and no-code PHP development.</p>');
 
+    print('This is a test');
     }
 }
 
