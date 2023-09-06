166ac5a1672e90c106839dc9d24acc512ffc6965
Janusz Tylek <j@epe.si>
2023-09-06 02:37:33 +0200

    Update HelloWorld_0.php

--- HelloWorld_0.php
+++ HelloWorld_0.php
@@ -9,7 +9,7 @@ class Academy_HelloWorld extends Module { // - notice how the class name represe
     print('<H1>Hello Janusz!</H1><H2>Welcome to Epesi Academy.</H2>');
     print('<p><strong>### Lesson 1 ###</strong><br><br>Hello World in <a href="https://epe.si">Epesi</a> to demonstrate low-code and no-code PHP development.</p>');
 
-    print('This is a test');
+    print('Hello World');
     }
 }
 
