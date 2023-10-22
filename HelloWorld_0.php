09ee10978235adc16f400826e0e04bc2381cd62b
Janusz Tylek <j@epe.si>
2023-10-22 14:20:11 +0200

    Update HelloWorld_0.php
     public function body() { // - modules main code
        $text = 'Hello World'; // - some text data
        $value = 42; // - some numeric data
        $table = array( // - some table data
          array('Name', 'Age'),
          array('Alice', 25),
          array('Bob', 30),
        );
        $this->display($text); // - display text data
        $this->display($value); // - display numeric data
        $this->display($table); // - display table data
        }

--- HelloWorld_0.php
+++ HelloWorld_0.php
@@ -1,17 +1,21 @@
 <?php
-
 defined("_VALID_ACCESS") || die('Direct access forbidden'); // - security feature
 
-class Academy_HelloWorld extends Module { // - notice how the class name represents its path
+class Academy_HelloWorld extends Module { 
+  // - notice how the class name represents its path
 
-  public function body() { // - modules main code
-
-    print('<H1>Hello Janusz!</H1><H2>Welcome to Epesi Academy.</H2>');
-    print('<p><strong>### Lesson 1 ###</strong><br><br>Hello World in <a href="https://epe.si">Epesi</a> to demonstrate low-code and no-code PHP development.</p>');
-
-    print('Hello World');
-    print('<BR><BR>It\'s easy, it\'s <STRONG>Epesi!</STRONG>');
+  public function body() { 
+    // - modules main view code
+    $text = 'Hello World'; // - some text data
+    $value = 42; // - some numeric data
+    $table = array( // - some table data
+      array('Name', 'Age'),
+      array('Alice', 25),
+      array('Bob', 30),
+    );
+    $this->display($text); // - display text data
+    $this->display($value); // - display numeric data
+    $this->display($table); // - display table data
     }
 }
-
 ?>
