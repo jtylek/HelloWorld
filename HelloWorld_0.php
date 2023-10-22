fb6b2f96792389a7e6c2f32fea25595e38c20b28
Janusz Tylek <j@epe.si>
2023-10-22 14:52:40 +0200

    Update HelloWorld_0.php
    Reverted to original single line

--- HelloWorld_0.php
+++ HelloWorld_0.php
@@ -5,20 +5,6 @@ class Academy_HelloWorld extends Module {
   // - notice how the class name represents its path
 
   public function body() { 
-    // - modules main view code
-    $text = 'Hello World'; // - some text data
-    $value = 42; // - some numeric data
-    $table = array( // - some table data
-      array('Name', 'Age'),
-      array('Alice', 25),
-      array('Bob', 30),
-    );
-    
-    print($text);
-    print('<BR>');
-    print($value);
-    print('<BR>');
-    var_dump($table);
-  }
-}
+         print('Hello World!');
+        }
 ?>
